4d75e21264fa566b98ba7d22379fd359b94d3fc2
Ahmed Bodiwala <ahmed_bodi@msn.com>
2024-05-04 19:56:58 +0100

    fix: Fix Exclude Paths across os's

--- setup.py
+++ setup.py
@@ -4,12 +4,16 @@ from glob import glob
 from setuptools import setup, Extension
 
 ROOT_DIR = os.path.dirname(__file__)
+if ROOT_DIR == '':
+  ROOT_DIR = '.'
+  
 SOURCES = [y for x in os.walk(ROOT_DIR) for y in glob(os.path.join(x[0], '*.c'))]
 INCLUDE_DIRS = [os.path.join(ROOT_DIR, o) for o in os.listdir(ROOT_DIR) if os.path.isdir(os.path.join(ROOT_DIR, o))]
 EXCLUDE_SOURCES = [
     os.path.join(ROOT_DIR, 'src', 'sha3', 'haval_helper.c'),
     os.path.join(ROOT_DIR, 'src', 'sha3', 'md_helper.c'),
 ]
+
 extensions = [
 	Extension(
 		"algomodule",
