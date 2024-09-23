374e369666dd2463731e70069f205b1f994e9a8c
Ahmed Bodiwala <ahmed_bodi@msn.com>
2024-09-24 00:49:56 +0100

    refactor: Switch to Extension based algorithms

--- setup.py
+++ setup.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 import os
+import sys
 from glob import glob
 from setuptools import setup, Extension
 
@@ -7,29 +8,61 @@ ROOT_DIR = os.path.dirname(__file__)
 if ROOT_DIR == '':
   ROOT_DIR = '.'
 
-C_SOURCES = [y for x in os.walk(ROOT_DIR) for y in glob(os.path.join(x[0], '*.c'))]
-CPP_SOURCES = [y for x in os.walk(ROOT_DIR) for y in glob(os.path.join(x[0], '*.cpp'))]
-SOURCES = C_SOURCES + CPP_SOURCES
-INCLUDE_DIRS = [os.path.join(ROOT_DIR, o) for o in os.listdir(ROOT_DIR) if os.path.isdir(os.path.join(ROOT_DIR, o))]
-EXCLUDE_SOURCES = [
-    os.path.join(ROOT_DIR, 'src', 'sha3', 'haval_helper.c'),
-    os.path.join(ROOT_DIR, 'src', 'sha3', 'md_helper.c'),
-]
 
-extensions = [
-    Extension(
-        "algomodule",
-        include_dirs=INCLUDE_DIRS,
+def scandir(dir, files=[]):
+    for file in os.listdir(dir):
+        path = os.path.join(dir, file)
+        if os.path.isfile(path) and path.endswith(".pyx"):
+            files.append(path.replace(os.path.sep, ".")[:-4])
+        elif os.path.isdir(path):
+            scandir(path, files)
+    return files
+
+
+# generate an Extension object from its dotted name
+def makeExtension(extName):
+    EXT_DIR = extName.replace(".", os.path.sep)
+    SOURCES =  [y for x in os.walk(extName.replace(".", os.path.sep)) for y in glob(os.path.join(x[0], '*.c'))]
+    SOURCES += [y for x in os.walk(extName.replace(".", os.path.sep)) for y in glob(os.path.join(x[0], '*.cpp'))]
+    EXCLUDE_SOURCES = [
+        os.path.join(EXT_DIR, 'sha3', 'haval_helper.c'),
+        os.path.join(EXT_DIR, 'sha3', 'md_helper.c'),
+    ]
+
+    LANGUAGE = None,
+    EXTRA_COMPILE_ARGS = []
+    
+    is_cpp = [item for item in SOURCES if any([word in item for word in ['cpp']])]
+    if is_cpp:
+        print(SOURCES)
+        LANGUAGE = 'c++'
+        EXTRA_COMPILE_ARGS = ['-std=c++11']
+        SOURCES += [EXT_DIR+'.cpp']
+    else:
+        SOURCES += [EXT_DIR+'.c']
+
+    print(SOURCES)
+    return Extension(
+        extName,
+        include_dirs = ["."],
         sources=list(filter(lambda x: x not in EXCLUDE_SOURCES, SOURCES)),
-        extra_compile_args=["-std=c++11"],
-        language="c++",
+        extra_compile_args=EXTRA_COMPILE_ARGS,
+        language=LANGUAGE,
     )
-]
+
+extNames = scandir("algomodule")
+extensions = [makeExtension(name) for name in extNames]
 
 setup(
     name = "algomodule",
     url = "https://github.com/electrum-altcoin/algomodule",
     author = "Ahmed Bodiwala",
     author_email = "ahmedbodi@crypto-expert.com",
+    packages = [
+      'algomodule',
+      'algomodule.core',
+      'algomodule.meraki',
+    ],
     ext_modules=extensions,
+    #cmdclass = {'build_ext': build_ext},
 )
--- test.py
+++ test.py
@@ -2,10 +2,19 @@ from binascii import unhexlify, hexlify
 import weakref
 import binascii
 import unittest
-import algomodule
+import algomodule.meraki
+
+algomodule.meraki._meraki_hash
+print(dir(algomodule.meraki))
+
+header_hash = b'\xa0\xb4\x06RU\xf0\xdd\xee\xeb\x0e\xbc\xf4\xd7/H\x16\x9b\xac\xe3\x06\x1fu\x1e\xfb\x15\x11]3\x88\x17a?'
+mix_hash = b'\xc2K\x92;s\xe8o\x84M\xccN\x90\x0b\x85Z\xb7\x06\x90\x96\xc5\x9ab\x8d\xc7\xbd\xc1\x9bD\x1d\xac\x08\xe1'
+nonce = 14613617863994149989
+
+print(algomodule.meraki._meraki_hash(header_hash, mix_hash, nonce))
 
 teststart = '700000005d385ba114d079970b29a9418fd0549e7d68a95c7f168621a314201000000000578586d149fd07b22f3a8a347c516de7052f034d2b76ff68e0d6ecff9b77a45489e3fd511732011df0731000'
 testbin = unhexlify(teststart)
 bin_len = len(testbin)
 
-print(algomodule._quark_hash(testbin))
+print(algomodule.core._quark_hash(testbin))
